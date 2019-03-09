080af449927c13f5ccf364e94d44b83d13ddd891
Josef Šimánek <josef.simanek@gmail.com>
2019-03-09 04:28:11 +0100

    Use custom headless chrome configuration.

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,13 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
+  Capybara.register_driver :headless_chrome do |app|
+    Capybara::Selenium::Driver.new app,
+      browser: :chrome,
+      desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(
+        chromeOptions: { args: %w(headless disable-gpu) }
+      )
+  end
+
+  driven_by :headless_chrome
 end
