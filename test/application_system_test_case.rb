31b5c5a1a35cf7ce4cab5f31718bdd038388faf0
Josef Šimánek <josef.simanek@gmail.com>
2019-03-09 04:50:52 +0100

    Use remote chrome provided by selenium hub when available.

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,12 +2,29 @@ require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   Capybara.register_driver :headless_chrome do |app|
-    Capybara::Selenium::Driver.new app,
-      browser: :chrome,
-      desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(
-        chromeOptions: { args: %w(headless disable-gpu) }
-      )
+    if ENV['SELENIUM_HUB_URL']
+      Capybara::Selenium::Driver.new app,
+        url: ENV['SELENIUM_HUB_URL'],
+        browser: :remote,
+        desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(
+          chromeOptions: { args: %w(headless disable-gpu) }
+        )
+    else
+      Capybara::Selenium::Driver.new app,
+        browser: :chrome,
+        desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(
+          chromeOptions: { args: %w(headless disable-gpu) }
+        )
+    end
   end
 
   driven_by :headless_chrome
+
+  setup do
+    if ENV['TEST_APP_HOST']
+      Capybara.server_port = ENV['TEST_APP_PORT'].to_i
+      Capybara.app_host = "http://#{ENV['TEST_APP_HOST']}:#{ENV['TEST_APP_PORT']}"
+      Capybara.server_host = '0.0.0.0'
+    end
+  end
 end
